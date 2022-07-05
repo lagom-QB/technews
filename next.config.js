63477797ed2ba14b1ee26c0ea8d5d8ece58207c4
Tebid Quinsy <28558929+lagom-QB@users.noreply.github.com>
2022-07-06 00:05:28 +0200

    changes happening ...

--- next.config.js
+++ next.config.js
@@ -1,27 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const withPlugins = require("next-compose-plugins");
-const optimizedImages = require("next-optimized-images");
-module.exports = withPlugins([
-  [
-    optimizedImages,
-    {
-      mozjpeg: {
-        quality: 80,
-      },
-      pngquant: {
-        speed: 3,
-        strip: true,
-        verbose: true,
-      },
-      imagesPublicPath: "/wallisconsultancy/_next/static/images/",
-    },
-  ],
-  {
-    assetPrefix: "/technews/",
-    basePath: "/technews",
-    env,
-    reactStrictMode: true,
-    concurrentFeatures: true,
-  },
-]);
+module.exports = {
+  reactStrictMode: true,
+  assetPrefix: "/technews/",
+  basePath: "/technews",
+};

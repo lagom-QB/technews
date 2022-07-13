65bbeacbbb0dd4ec07bfc17ab3389efe20c74916
lagom-QB <28558929+lagom-QB@users.noreply.github.com>
2022-07-13 00:49:56 +0000

    Deploying to gh-pages from @ lagom-QB/technews@104ba8588863e11f61cf5d4896995378125d5a46 🚀

--- _next/static/FD3ABimso9uRT35rS9jzr/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/103-4ce3b8ce1e4d3305.js","static/css/d083c43168d0dd75.css","static/chunks/pages/index-7280d7eac7210ee2.js"],"/_error":["static/chunks/pages/_error-777a5f0e462a9bdf.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
